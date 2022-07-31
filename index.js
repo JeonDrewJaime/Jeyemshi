58f2f70faa49c2225c560e3b90068b1f9a0e3081
Jeon Drew Jaime <jaimejeondrew@gmail.com>
2022-07-31 21:09:09 +0800

    ios

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var loader = document.getElementById("loader");
 
-window.addEventListener("load" , function(){
-   loader.style.display = "none";
+
+$(window).on("load" , function(){
+   $("#loader").fadeOut("slow");
 });
